5e0118deea68f9fd15f8f67423cb306c36f95976
sushantsharmass <sushantsharma4500@gmail.com>
2024-02-06 14:31:48 +0530

    style: :art: Converts Cloud model into jsx File
    Added cloud jsx file and adds opacity to the clouds
    
    Resolve #6

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { Float, OrbitControls } from "@react-three/drei";
+import { Cloud, Float, OrbitControls } from "@react-three/drei";
 import { Background } from "./Background";
 import { Airplane } from "./Airplane";
 
@@ -14,6 +14,11 @@ export const Experience = () => {
       position-y={0.1}
       />
       </Float>
+      <Cloud
+      opacity={0.5}
+      scale={[0.3, 0.3, 0.3]}
+      position={[-2, 1, -3]}
+      />
     </>
   );
 };
