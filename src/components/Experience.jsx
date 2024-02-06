249bd86ff651570bd9f9436385b7380f622dd308
sushantsharmass <sushantsharma4500@gmail.com>
2024-02-06 23:25:08 +0530

    feat: :sparkles: Adds vector three line to the scene

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,38 @@
-import { Float, OrbitControls } from "@react-three/drei";
+import { Float, Line, OrbitControls } from "@react-three/drei";
 import { Background } from "./Background";
 import { Airplane } from "./Airplane";
 import { Cloud } from "./Cloud";
+import { useMemo } from "react";
+import * as THREE from "three"
 
+const LINE_NB_POINTS = 2000;
 
 export const Experience = () => {
+
+  const curve = useMemo(()=>{
+    return new THREE.CatmullRomCurve3([
+      new THREE.Vector3(0, 0, 0),
+      new THREE.Vector3(0, 0, -10),
+      new THREE.Vector3(-2, 0, -20),
+      new THREE.Vector3(-3, 0, -30),
+      new THREE.Vector3(0, 0, -40),
+      new THREE.Vector3(5, 0, -50),
+      new THREE.Vector3(7, 0, -60),
+      new THREE.Vector3(5, 0, -70),
+      new THREE.Vector3(0, 0, -80),
+      new THREE.Vector3(0, 0, -90),
+      new THREE.Vector3(0, 0, -100),
+    ],
+    false,
+    "catmullrom",
+    0.5)
+  })
+
+  const linepoints = useMemo(() => {
+    return curve.getPoints(LINE_NB_POINTS);
+  },[curve])
+
+
   return (
     <>
       <OrbitControls />
@@ -23,6 +51,16 @@ export const Experience = () => {
       scale={[0.3,0.3,0.3]}
       position={[-1,1,-3]}
       /> */}
+
+      <group position-y={-2}>
+      <Line
+      points={linepoints}
+      color={"white"}
+      opacity={0.7}
+      transparent
+      lineWidth={16}/>
+      </group>
+
       <Cloud opacity={0.5} scale={[0.3, 0.3, 0.3]} position={[-2, 1, -3]} />
       <Cloud opacity={0.5} scale={[0.2, 0.3, 0.4]} position={[1.5, -0.5, -2]} />
       <Cloud
