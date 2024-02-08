84ad466e1de613068692d48516726dd7fe060bde
sushantsharmass <sushantsharma4500@gmail.com>
2024-02-08 17:21:28 +0530

    fix: :art: Little changes in the backgroud of the scene

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,25 +4,40 @@ import { Gradient, LayerMaterial } from "lamina";
 import * as THREE from "three";
 
 export const Background = () => {
+  const colorA = "#0923be";
+  const colorB = "#ffad30";
+  const start = 0.2;
+  const end = -0.5;
   return (
     <>
-      <Environment preset="sunset" />
-      <Sphere scale={[100, 100, 100]} rotation-y={Math.PI / 2}>
-        <LayerMaterial
-          color={"#ffffff"}
-          lighting="physical"
-          transmission={1}
-          side={THREE.BackSide}
-        >
+      <Sphere scale={[500, 500, 500]} rotation-y={Math.PI / 2}>
+        <LayerMaterial color={"#ffffff"} side={THREE.BackSide}>
           <Gradient
-            colorA={"#357ca1"}
-            colorB={"white"}
+            colorA={colorA}
+            colorB={colorB}
             axes={"y"}
-            start={0}
-            end={-0.5}
+            start={start}
+            end={end}
           />
         </LayerMaterial>
       </Sphere>
+      <Environment resolution={256}>
+        <Sphere
+          scale={[100, 100, 100]}
+          rotation-y={Math.PI / 2}
+          rotation-x={Math.PI}
+        >
+          <LayerMaterial color={"#ffffff"} side={THREE.BackSide}>
+            <Gradient
+              colorA={colorA}
+              colorB={colorB}
+              axes={"y"}
+              start={start}
+              end={end}
+            />
+          </LayerMaterial>
+        </Sphere>
+      </Environment>
     </>
   );
 };
