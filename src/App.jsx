50284802ec650f48ba1117ed406b375b878ce36c
sushantsharmass <sushantsharma4500@gmail.com>
2024-02-08 17:05:52 +0530

    feat: :bento: Adds Fonts file to the project

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,7 @@ function App() {
 
   return (
     <>
-     <Canvas
-    //  camera={{
-    //   position: [0, 0, 5],
-    //   fov: 30,
-    // }}
-     >
-      {/* <directionalLight/>
-      <ambientLight/> */}
+     <Canvas>
       <color attach="background" args={["#ececec"]} />
           <ScrollControls pages={5} damping={1}>
           <Experience />
